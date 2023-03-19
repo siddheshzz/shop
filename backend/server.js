0f003e899f0718e9a24d31b9454fc3259926e2f9
siddhesh zope <siddheshzope1023@gmail.com>
2023-03-19 23:58:32 +0530

    Cosmetic changes in UI and added static path to build folder for deployment

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,14 @@ connectDB()
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-})
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+
+// app.get('/', (req, res) => {
+//     res.send('API is running...')
+// })
 
 app.use(express.json())
 
@@ -32,7 +37,17 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
 
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running....')
+    })
+}
 
 
 app.use(notFound)
